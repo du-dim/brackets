77fbab03398eb4d9594ad48e86b6f32677462e07
du-dim <dim-dubovik@yandex.ru>
2021-08-05 03:38:40 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, arr) {
+  
+  let len_a = arr.length;
+	let len_s = str.length;
+	
+	str = arr.map(a => str = str.replace(`${a[0]}${a[1]}`, ''))[len_a-1];
+  return (str.length === 0) ? true : (str.length === len_s) ? false : check(str, arr); 
+	}
